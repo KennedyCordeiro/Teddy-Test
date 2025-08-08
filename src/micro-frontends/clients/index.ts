c31de9718a9dbae8946352a61de19c8b6d654d58
Kennedy Cordeiro <kennedysenju@gmail.com>
2025-08-08 16:21:24 -0300

    fix: Correção de importação

--- src/micro-frontends/clients/index.ts
+++ src/micro-frontends/clients/index.ts
@@ -1,2 +1,4 @@
-export { ClientList } from './components/ClientList/ClientList';
-export { SelectedClients } from './components/SelectedClients/SelectedClients'; 
+export { ClientList } from "./components/ClientList/ClientList";
+export { SelectedClients } from "./components/SelectedClients/SelectedClients";
+export { CreateClient } from "./components/CreateClient/CreateClient";
+export { ConfirmDeleteModal } from "./components/ConfirmDeleteModal";
